a794f757b3f00da4f51ddbc5f636d7f1c6ffac6f
Rodolphe Desbois <rodolphedesbois@gmail.com>
2022-11-15 15:03:30 +0100

    Exercice Done

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Create
+  get "tasks/new", to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create"
+  # Read all
+  get "tasks", to: "tasks#index"
+  # Read one - The `show` route needs to be *after* `new` route.
+  get "tasks/:id", to: "tasks#show", as: :task
+  # Edit
+  get "tasks/:id/edit", to: "tasks#edit"
+  # Update
+  patch "tasks/:id", to: "tasks#update"
+  # Destroy
+  delete "tasks/:id", to: "tasks#destroy"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_15_101049) do
-  create_table "restaurants", force: :cascade do |t|
+ActiveRecord::Schema[7.0].define(version: 2022_11_15_110135) do
+  create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
     t.boolean "completed", default: false
